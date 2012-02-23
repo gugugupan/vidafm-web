1bbebf6bc38bb6cebd4a279d3c7dec47f223f016
stranbird <meteey@gmail.com>
2012-02-23 20:25:48 +0800

    1. activity show的侧边栏。

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,4 +7,18 @@ class ActivitiesController < ApplicationController
   	@activities = Activity.fetchLikeList(params[:id])
   end
 
+  def show
+  	kMomentID = 13666
+  	kActivityID = 97056
+  	@moment = Moment.fetch(kMomentID, :activity_id => kActivityID, :page => params[:page], :page_size => 4)
+    @moment['current_page'] = 0 if @moment['current_page'].to_i == -1 # 如果没有找到activity，后台返回当前页数是-1
+    @activity = Object.new
+    @moment['data']['items'].each do |e|
+  		if e['id'] == kActivityID
+  			@activity = e
+  			break
+  		end
+    end
+  end
+
 end
--- app/models/moment.rb
+++ app/models/moment.rb
@@ -10,7 +10,10 @@ class Moment < ActiveRecord::Base
 
   def self.fetch(id, options = {})
     options.reject! { |key, value| value.nil? } # 清理为空的键值对，因为如果同时出现activity_id和page，服务器不认page。
-    JSON.parse VIDA.call("moment/show/#{id}?page_size=#{options['page_size'] || PAGE_SIZE}&offset_padding=0&#{options.to_query}", nil, options[:current_user])
+    r = JSON.parse(VIDA.call("moment/show/#{id}?page_size=#{options['page_size'] || PAGE_SIZE}&offset_padding=0&#{options.to_query}", nil, options[:current_user]))
+    p '------'
+    p r
+    r
   end
 
   def self.fetch_by_user_id_and_year_and_month(options = {})
