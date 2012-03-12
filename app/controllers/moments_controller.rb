78e1eda5cb9d260398538bd391d34bc61ec350d3
stranbird <meteey@gmail.com>
2012-03-12 14:10:56 +0800

    fixed #31, limits page_size upto 6

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -7,7 +7,7 @@ class MomentsController < ApplicationController
 
     params[:page] = params[:page].to_i - 1 if params[:page]
 
-    params[:page_size] = 6 if params[:page_size].blank?
+    params[:page_size] = 6 if (params[:page_size].blank? or params[:page_size].to_i > 6)
 
     @moment = Moment.fetch(params[:id], :activity_id => params[:activity_id], :page => params[:page], :page_size => params[:page_size], :current_user => current_user)
     @moment['current_page'] = 0 if @moment['current_page'].to_i == -1 # 如果没有找到activity，后台返回当前页数是-1 
