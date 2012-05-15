babb402dec6fd275e53f6cdd7b9c05c4ac5c25dd
stranbird <meteey@gmail.com>
2012-05-15 16:07:20 +0800

    modified assets host to static.vida.fm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Bluerain::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+
+  config.action_controller.asset_host = "static.vida.fm"
 end
