3952b8f2b141a12251cd8239f2db2c6905de87fe
stranbird <meteey@gmail.com>
2012-06-07 15:08:44 +0800

    updated feedback logic

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -3,10 +3,8 @@ require "VIDA"
 class Feedback < ActiveRecord::Base
     class << self
         def create(options)
-            o = options
-                .stringify_keys
-                .keep_if { |v| ['from', 'content'].include? v[0] }
-                
+            o = options.stringify_keys.keep_if { |k, v| ['from', 'content'].include? k }
+
             JSON.parse VIDA.call("feedback/new", o, nil)
         end
         
