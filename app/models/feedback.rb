1dd4f5d099b135dfa29f6b89f1b5bd1e796369db
stranbird <meteey@gmail.com>
2012-06-07 15:44:52 +0800

    completely removed active-record

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,7 +4,6 @@ class Feedback
     class << self
         def create(options)
             JSON.parse VIDA.call("feedback/new", options, nil)
-        end
-        
+        end        
     end
 end
