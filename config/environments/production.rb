0ca61098d935c3746fb4f3fad778076bd0ef2737
gugugupan <gugugupan@163.com>
2013-04-15 19:17:13 +0800

    bug fix for image assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Bluerain::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   ######config.assets.precompile += %w( activities.js moments.js  )
+  config .assets .precompile += %w( slideshow.css slideshow.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
