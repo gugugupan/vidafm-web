e3cf4ff6ef1568c9ad4846989d51441b14324ef4
gugugupan <gugugupan@163.com>
2013-04-24 22:45:25 +0800

    bug fix!

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -551,3 +551,17 @@ function showCenterBox( str )
 		$select .fadeIn( 400 , function() { setTimeout( function() { $select .remove() ; } , 1000 ) } ) ;
 	}
 }
+
+// Download GA
+function gaDownload()
+{
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-19458693-4']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+}
